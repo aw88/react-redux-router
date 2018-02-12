ef5d4309af2e2a4b870b4bd382ef7d244d4da5a4
Alex W <alex.w.walton@gmail.com>
2018-02-12 19:34:15 +0000

    Adding DefinitionListItem component

---
+++ src/components/DefinitionListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DefinitionListItem = ({ definition }) => (
+  <div className="DefinitionListItem">
+    <h4>{ definition.title }</h4>
+  </div>
+);
+
+export default DefinitionListItem;
