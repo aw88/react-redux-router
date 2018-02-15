637d4f7dd56839562c848dbcb9d793d4e98bf900
Alex W <alex.w.walton@gmail.com>
2018-02-15 21:25:40 +0000

    Adding Redux devtools setup

--- src/index.js
+++ src/index.js
@@ -7,18 +7,24 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import definitionsApp from './reducers';
+import Definition from './models/Definition';
 
-const store = createStore(definitionsApp);
+const store = createStore(
+  definitionsApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 if (process.env.NODE_ENV === 'development') {
   store.subscribe(() => {
     console.log(store.getState());
   });
 
-  const { addDefinition } = require('./actions');
-  store.dispatch(addDefinition('First definition'))
-  store.dispatch(addDefinition('Second definition'))
-  store.dispatch(addDefinition('Third definition'))
+  const { addDefinition, updateDefinition } = require('./actions');
+  store.dispatch(addDefinition('First definition'));
+  store.dispatch(addDefinition('Second definition'));
+  store.dispatch(addDefinition('Third definition'));
+
+  store.dispatch(updateDefinition(1, new Definition(1, 'First definition', ['term1', 'term2'], ['term3'])));
 }
 
 ReactDOM.render(
